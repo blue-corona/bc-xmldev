8db09e2eb48eac354dc70c1e16d95865d8cb7b90
unknown <piyush.singh@twinspark.co>
2020-05-02 15:54:05 +0530

    Video section created

--- wp-content/themes/bc-xml/page-templates/bc-homepage.php
+++ wp-content/themes/bc-xml/page-templates/bc-homepage.php
@@ -24,11 +24,14 @@ get_header();
             <div class="row text-center text-lg-center text-md-center ">
                 <div class="offset-md-2 col-md-8 col-12 offset-lg-2 col-lg-8 col-sm-12 px-3">
                     <h3 class="position-relative bottom_line mb-5">What We Believe</h3>
-                    <div class="mx-5 what_we_video position-relative bg-white overflow-hidden" data-toggle="modal" data-target="#exampleModal">
+                    <div class="mx-5 position-relative bg-white overflow-hidden what_we_video" data-toggle="modal" data-target="#exampleModal">
                         <div class="row">
                             <div class="offset-2 col-md-8 mt-n3 pb-5">
-                                <span class="position-absolute"><i class="fas fa-caret-right text-white"></i></span>
+                                <span class="position-absolute play_icon p-3 text-center">
+                                    <i class="fas fa-caret-right"></i>
+                                </span>
                                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/what-we-icon.png" class="img-fluid w-100 mx-auto mt-n5">
+
                             </div>
                         </div>
                     </div>
