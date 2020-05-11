30d12c9cd767d34f2eb43c0e8a2a7afd48b2aeed
unknown <piyush.singh@twinspark.co>
2020-05-11 18:02:37 +0530

    Text Styling #96

--- wp-content/themes/bc-xml/page-templates/bc-homepage.php
+++ wp-content/themes/bc-xml/page-templates/bc-homepage.php
@@ -71,7 +71,7 @@ get_header();
                               <div class="row align-items-center">
                                     <div class="offset-md-1 col-md-5 col-12">
                                         <div class="pl-md-5 text-center text-md-left">
-                                            <p class="text-uppercase">team driven<span class="d-block bc_color_primary bc_text_40 bc_text_semibold bc_line_height_38">investing</span></p>
+                                            <p class="text-uppercase bc_text_20">team driven<span class="d-block bc_color_primary bc_text_40 bc_text_semibold bc_line_height_38">investing</span></p>
                                             <p>“No one knows everything.” We try to make informed, rational decisions that align your investments with your lifestyle.</p>
                                             <a href="#" class="btn_primary py-2 px-3 mt-4">LEARN HOW</a>
                                         </div>
@@ -87,7 +87,7 @@ get_header();
                               <div class="row align-items-center">
                                     <div class="offset-md-1 col-md-5 col-12">
                                         <div class="pl-md-5 text-center text-md-left">
-                                            <p class="text-uppercase">360<span class="d-block bc_color_primary bc_text_40 bc_text_semibold bc_line_height_38">SUPPORT</span></p>
+                                            <p class="text-uppercase bc_text_20">360<span class="d-block bc_color_primary bc_text_40 bc_text_semibold bc_line_height_38">SUPPORT</span></p>
                                             <p>“We have a person for that”. If it’s important to you, we are here to help, beyond just investments and planning.</p>
                                             <a href="#" class="btn_primary py-2 px-3 mt-4">LEARN HOW</a>
                                         </div>
@@ -103,7 +103,7 @@ get_header();
                               <div class="row align-items-center">
                                     <div class="offset-md-1 col-md-5 col-12">
                                         <div class="pl-md-5 text-center text-md-left">
-                                            <p class="text-uppercase"><span class="d-block bc_color_primary bc_text_40 bc_text_semibold bc_line_height_38">QUALIFIED</span></p>
+                                            <p class="text-uppercase bc_text_20"><span class="d-block bc_color_primary bc_text_40 bc_text_semibold bc_line_height_38">QUALIFIED</span></p>
                                             <p>“We’ve got the credentials.”</p>
                                             <a href="#" class="btn_primary py-2 px-3 mt-4">Meet the team</a>
                                         </div>
@@ -119,7 +119,7 @@ get_header();
                               <div class="row align-items-center">
                                     <div class="offset-md-1 col-md-5 col-12">
                                         <div class="pl-md-5 text-center text-md-left">
-                                            <p class="text-uppercase">Community<span class="d-block bc_color_primary bc_text_40 bc_text_semibold bc_line_height_38">CENTRIC</span></p>
+                                            <p class="text-uppercase bc_text_20">Community<span class="d-block bc_color_primary bc_text_40 bc_text_semibold bc_line_height_38">CENTRIC</span></p>
                                             <p>“Our clients are not clones.”</p>
                                             <a href="#" class="btn_primary py-2 px-3 mt-4">Learn more about how we advise our community</a>
                                         </div>
@@ -135,7 +135,7 @@ get_header();
                               <div class="row align-items-center">
                                     <div class="offset-md-1 col-md-5 col-12">
                                         <div class="pl-md-5 text-center text-md-left">
-                                            <p class="text-uppercase"><span class="d-block bc_color_primary bc_text_40 bc_text_semibold bc_line_height_38">INDEPENDENT</span></p>
+                                            <p class="text-uppercase bc_text_20"><span class="d-block bc_color_primary bc_text_40 bc_text_semibold bc_line_height_38">INDEPENDENT</span></p>
                                             <p>“We think for ourselves.”</p>
                                             <a href="#" class="btn_primary py-2 px-3 mt-4">Read about our beliefs and how we invest in our community</a>
                                         </div>
