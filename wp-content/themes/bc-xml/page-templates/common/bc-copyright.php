0fe22f8f3c380cf4903b5bf2ebb3391d99d56aa0
Humayun Khan <humayun.khan@twinspark.co>
2020-05-12 11:23:47 +0530

    completed #84,#85,#87,#88,#105,#107,#108

--- wp-content/themes/bc-xml/page-templates/common/bc-copyright.php
+++ wp-content/themes/bc-xml/page-templates/common/bc-copyright.php
@@ -6,13 +6,15 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
-<?php echo bc_get_theme_mod('bc_theme_options', 'footer','copyright_text', '<div class=" container-fluid" style="background: #e3e4e9;">
-  	<div class="container bc_text_gray_color pb-5 py-4 py-lg-0 pb-md-0 px-0 bc_color_primary text-center text-md-left bc_color_gray bc_text_16 bc_sm_text_13 bc_line_height_50 bc_sm_line_height_18 bc_text_normal bc_font_alt_1">
-  		 <span class="bc_text_18 bc_sm_text_13 bc_text_heavy"> © </span> '.date("Y").'  XML FINANCIAL GROUP | </span>
-       <span class="d-lg-inline-block bc_color_primary --imp">WEB DESIGN BY
-		   <a class="bc_color_primary bc_color_quinary_hover bc_text_normal no_hover_underline bc_text_16 bc_sm_text_13" href="https://www.bluecorona.com" target="_blank"><img src="'.get_template_directory_uri().'/img/bc_logo.png">&nbsp;Blue Corona</a> </span>
-		  <span class="d-inline-block "> |</span>
-      <a class="bc_color_primary bc_color_quinary_hover bc_text_normal no_hover_underline bc_text_16 bc_sm_text_13" href="#" data-toggle="modal" data-target="#disclaimer">Disclaimer</a>
+<?php echo bc_get_theme_mod('bc_theme_options', 'footer','copyright_text', '<div class=" container-fluid pb-3" style="background: #e3e4e9;">
+  	<div class="container pb-5 pt-2 py-lg-0 pb-md-0 px-0 text-center text-md-left">
+  		 <div class="bc_text_18 bc_color_primary bc_sm_text_13 bc_text_16 bc_line_height_50 bc_sm_line_height_18 bc_text_normal bc_font_alt_1 bp-3">
+          <span class="bc_text_18 bc_color_primary bc_sm_text_13 bc_text_heavy bc_text_16 bc_line_height_50 bc_sm_line_height_18 bc_text_normal bc_font_alt_1"> © </span> '.date("Y").'  XML FINANCIAL GROUP      <span class="d-none d-lg-inline-block"> | </span> <br class="d-lg-none"> </span>
+          <span class="d-lg-inline-block bc_color_primary --imp bc_text_16 bc_line_height_50 bc_sm_text_13 bc_sm_line_height_18 bc_text_normal bc_font_alt_1"> WEB DESIGN BY
+          <a class="bc_color_primary bc_color_quinary_hover bc_text_normal no_hover_underline" href="https://www.bluecorona.com" target="_blank"><img src="'.get_template_directory_uri().'/img/bc_logo.png">&nbsp;Blue Corona</a> </span>
+          <span class="d-inline-block bc_color_primary bc_sm_text_13 bc_text_16 bc_line_height_50 bc_sm_line_height_18 bc_text_normal bc_font_alt_1"> |</span>
+          <a class="bc_color_primary bc_color_quinary_hover bc_text_normal no_hover_underline bc_text_16 bc_line_height_50 bc_sm_text_13 bc_sm_line_height_18 bc_text_normal bc_font_alt_1" href="#" data-toggle="modal" data-target="#disclaimer">Disclaimer</a>
+       </div>
   	</div>
 </div>');?>
 
@@ -20,7 +22,7 @@ defined( 'ABSPATH' ) || exit;
     <div class="container p-0">
         <div class="row no-gutters">
             <div class="col-md-12 text-center">
-                <a href="#" data-toggle="modal" data-target="#myModal_mobile" class="px-3 py-3 no_hover_underline bc_font_default bc_text_24 bc_line_height_26 text-white bc_text_light bc_color_primary_hove bc_color_primary_bg d-block"> LET’S TALK</a>
+                <a href="#" data-toggle="modal" data-target="#myModal_mobile" class="px-3 py-3 no_hover_underline bc_font_default bc_text_22 bc_line_height_26 text-white bc_text_light bc_color_primary_hove bc_color_primary_bg d-block"> LET’S TALK</a>
             </div>
         </div>
     </div>
