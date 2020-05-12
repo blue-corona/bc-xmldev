a1b1a8ec7126dd25cd88604fe8c14d84e8a7f709
unknown <piyush.singh@twinspark.co>
2020-05-12 12:22:08 +0530

    Text styling/Formatting #100

--- wp-content/themes/bc-xml/page-templates/bc-homepage.php
+++ wp-content/themes/bc-xml/page-templates/bc-homepage.php
@@ -262,7 +262,7 @@ get_header();
             <div class="row no-gutters">
                 <div class="col-lg-10 px-lg-4 offset-lg-1 text-center">
                     <span class="position-relative bc_text_36 bc_line_height_36 mt-5 bc_font_default bc_text_thin text-white bottom_line1 d-block px-md-4 bc_sm_text_32 bc_sm_line_height_32">Wondering if there might be something more you and your money could do?</span>
-                    <span class="bc_text_20 bc_line_height_34 bc_sm_line_height_30 bc_font_alt_1 bc_text_light text-white d-block mt-4 pt-1 bc_sm_text_18">There are several different ways to connect with us. Chose whichever you are most <br> comfortable with. </span>
+                    <span class="bc_text_20 bc_line_height_34 bc_sm_line_height_30 bc_font_alt_1 bc_text_light text-white d-block mt-4 pt-1 bc_sm_text_18 px-md-4 px-2">There are several different ways to connect with us. Chose whichever you are most  comfortable with. </span>
                     <a href="#" class="btn_secondary px-5 py-3 mt-4 bc_line_height_26">let’s talk </a>
                 </div>
             </div>
