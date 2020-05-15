fbfbdc7d09e611442ab3f3154f18ffc8eedbe664
unknown <piyush.singh@twinspark.co>
2020-05-15 20:03:08 +0530

    team page and contact page issues resolved

--- wp-content/themes/bc-xml/page-templates/common/who-we-are.php
+++ wp-content/themes/bc-xml/page-templates/common/who-we-are.php
@@ -5,7 +5,7 @@
   <div class="container pt-5 pb-0 px-md-4">
     <div class="row pt-5">
       <div class="col-md-12 text-center text-lg-center text-md-center pt-5 pb-md-5"><h3 class="position-relative bottom_line pt-4 mb-5">Who We Are</h3></div>
-      <div class="col-xl-7 col-12 order-2 order-xl-1 mt-3 pb-5">
+      <div class="col-xl-7 col-12 order-2 order-xl-1 mt-3 pb-4">
         <!-- Swiper -->
         <div class="swiper-container who_we_swiper">
           <div class="swiper-wrapper mb-5">
@@ -31,9 +31,9 @@
                     <div class="image position-relative d-inline-block mx-auto p-2 image_border">
 
                       <?php if (isset($image) && !empty($image)){?>
-                        <img class="img-fluid" style="max-width:138px !important;" src="<?php echo $image ?>">
+                        <img class="img-fluid rounded-circle" style="max-width:138px !important;" src="<?php echo $image ?>">
                       <?php }else{
-                        echo '<img class="img-fluid" src="https://placehold.it/95x98">';
+                        echo '<img class="img-fluid rounded-circle" src="https://placehold.it/95x98">';
                       }?>
                     </div>
                   </div>
@@ -58,14 +58,14 @@
                         <div class="swiper-button-next who_we_next bc_color_primary bc_text_30 bc_text_light bc_line_height_60"><i class="fal fa-2x fa-angle-right"></i></div>
                         <div class="swiper-button-prev who_we_prev bc_color_primary bc_text_30 bc_text_light bc_line_height_60"><i class="fal fa-2x fa-angle-left"></i></div>
                       </div>
-                      <div class="fullteam d-md-none d-block text-center mb-5">
-                        <a  href="<?php echo get_home_url();?>/our-team/" class="btn_secondary px-4 py-2 mt-4 mb-5 ">FULL TEAM</a>
+                      <div class="fullteam d-md-none d-block text-center mb-0">
+                        <a  href="<?php echo get_home_url();?>/our-team/" class="btn_secondary px-4 py-2 mt-4 mb-0 ">FULL TEAM</a>
                       </div>
                     </div>
                     <div class="col-xl-5 order-1 order-xl-2 mt-3 pb-md-0 pb-3 text-center text-xl-left team_text px-md-5">
                       <div class="px-md-3">
                         <h2>A Team of Passionate Experts</h2>
-                        <p class="bc_sm_text_normal">With roots back to 1981 XML has been passionate about assembling a team of experts not just in money management but also in listening and in sincere care for our client's well being. We currently have CFPs, CRPCs, MBAs working to improve our Client's lives.</p>
+                        <p class="bc_sm_text_normal mt-4 mt-md-0">With roots back to 1981 XML has been passionate about assembling a team of experts not just in money management but also in listening and in sincere care for our client's well being. We currently have CFPs, CRPCs, MBAs working to improve our Client's lives.</p>
                         <a href="<?php echo get_home_url();?>/our-team/" class="btn_secondary px-4 py-2 mt-4 mb-0 d-md-inline-block d-none">FULL TEAM</a>
                       </div>
                     </div>
