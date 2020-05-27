858573c0bebf745dba08824d66b487a057b3afa5
Vishal Sharma <vsharma@bluecorona.com>
2020-05-27 11:52:26 +0530

    Community sub page
    #20321 ticket  - Rev4 and Rev8

--- wp-content/themes/bc-xml/page-templates/bc-communitiespage.php
+++ wp-content/themes/bc-xml/page-templates/bc-communitiespage.php
@@ -16,10 +16,6 @@ get_header();?>
                 <div class="col-lg-12">
                     <h1><?php the_title(); ?></h1>
 					<?php the_content(); ?>
-                    <div class="mt-5 py-5 border-top">
-                      <h2>Community Members</h2>
-                      <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat</p>
-                    </div>
                 </div> 
               <!-- Community Members -->
 				<?php 
