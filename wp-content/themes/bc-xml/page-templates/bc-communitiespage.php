d877fa6cb23351bdc40ec6cb879d9960f01d8225
Vishal Sharma <vsharma@bluecorona.com>
2020-05-27 11:33:26 +0530

    Community Sub page
    #20321 ticket  - Rev4 and Rev8

--- wp-content/themes/bc-xml/page-templates/bc-communitiespage.php
+++ wp-content/themes/bc-xml/page-templates/bc-communitiespage.php
@@ -14,24 +14,18 @@ get_header();?>
         <div class="container">
             <div class="row no-gutters">
                 <div class="col-lg-12">
-                    <h1>community Starting out - Solid Foundation</h1>
-                    <p>Great work considering how best to position yourself for the future. We get that yes, someday, you want to retire, but there are also things you want to do now. You aren’t living for a future retirement, but rather want to maximize your entire life. We can work thru that together, building on lessons learned from others in a similar situation, but making it your own.</p>
-                    <ul>
-                      <li> How to I balance retirement, investing, paying off debt and maybe buying a home? 
-                      <li> Which type of retirement account is right for me?</li>
-                      <li> How do we make sense of our company benefits?</li>
-                      <li> Do I really need to think about insurance?</li>
-                      <li> How does anyone afford to have kids?!</li>
-                      <li> Daycare is so expensive, should one of us stay home?</li>
-                      <li> Can I be a little frivolous and travel? </li>
-                    </ul>
+                    <h1><?php the_title(); ?></h1>
+					<?php the_content(); ?>
                     <div class="mt-5 py-5 border-top">
                       <h2>Community Members</h2>
                       <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat</p>
                     </div>
                 </div> 
               <!-- Community Members -->
-
+				<?php 
+						$team_members_ids = get_field( "team_members" ); 
+						if($team_members_ids){
+				?>
                 <div class="col-lg-12">
                   <div class="swiper-button-next members-button-next d-none d-lg-block">
                       <i class="far fa-chevron-right bc_text_30 bc_line_height_34 bc_color_primary"></i>
@@ -41,64 +35,67 @@ get_header();?>
                   </div>
                   <div class="swiper-container community-members-swiper mx-md-5 pb-3 mt-lg-5">
                     <div class="swiper-wrapper pb-2">
+					<?php foreach($team_members_ids as $team_member_id){ ?>
                       <div class="swiper-slide">
                         <div class="members px-3 pt-3 pb-4 border position-relative overflow-hidden">
                           <div class="img bc_color_lightgray_bg">
-                            
+                            <?php $member_img = wp_get_attachment_image_src( get_post_thumbnail_id( $team_member_id ),'full' ); ?>
+							<img  src="<?php echo $member_img[0]; ?>" />
                           </div>
-                          <span class="bc_font_alt_1 bc_text_22 bc_line_height_26 bc_text_bold bc_color_primary border-bottom pb-2 mt-3 d-block">Full Name</span>
-                          <span class="bc_font_alt_1 bc_text_20 bc_line_height_24 bc_text_normal bc_color_secondary my-2 d-block">Title</span>
+                          <span class="bc_font_alt_1 bc_text_18 bc_line_height_26 bc_text_bold bc_color_primary border-bottom pb-2 mt-3 d-block"><?php echo get_the_title($team_member_id); ?></span>
+                          <span class="bc_font_alt_1 bc_text_16 bc_line_height_24 bc_text_normal bc_color_secondary my-2 d-block"><?php echo get_post_meta( $team_member_id, 'team_position', true ); ?></span>
+						  <?php if(get_the_excerpt($team_member_id)){ ?>
                           <div class="position-absolute detail p-3">
-                            <span class="bc_text_18 bc_line_height_30 text-white bc_font_default bc_text_normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </span>
+                            <span class="bc_text_18 bc_line_height_30 text-white bc_font_default bc_text_normal"><?php echo get_the_excerpt($team_member_id); ?></span>
                           </div>
+						  <?php } ?>
                         </div>
                       </div>
-                      <div class="swiper-slide">
-                        <div class="members px-3 pt-3 pb-4 border position-relative overflow-hidden">
-                          <div class="img bc_color_lightgray_bg">
-                            
-                          </div>
-                          <span class="bc_font_alt_1 bc_text_22 bc_line_height_26 bc_text_bold bc_color_primary border-bottom pb-2 mt-3 d-block">Full Name</span>
-                          <span class="bc_font_alt_1 bc_text_20 bc_line_height_24 bc_text_normal bc_color_secondary my-2 d-block">Title</span>
-                          <div class="position-absolute detail p-3">
-                            <span class="bc_text_18 bc_line_height_30 text-white bc_font_default bc_text_normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </span>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="swiper-slide">
-                        <div class="members px-3 pt-3 pb-4 border position-relative overflow-hidden">
-                          <div class="img bc_color_lightgray_bg">
-                            
-                          </div>
-                          <span class="bc_font_alt_1 bc_text_22 bc_line_height_26 bc_text_bold bc_color_primary border-bottom pb-2 mt-3 d-block">Full Name</span>
-                          <span class="bc_font_alt_1 bc_text_20 bc_line_height_24 bc_text_normal bc_color_secondary my-2 d-block">Title</span>
-                          <div class="position-absolute detail p-3">
-                            <span class="bc_text_18 bc_line_height_30 text-white bc_font_default bc_text_normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </span>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="swiper-slide">
-                        <div class="members px-3 pt-3 pb-4 border position-relative overflow-hidden">
-                          <div class="img bc_color_lightgray_bg">
-                            
-                          </div>
-                          <span class="bc_font_alt_1 bc_text_22 bc_line_height_26 bc_text_bold bc_color_primary border-bottom pb-2 mt-3 d-block">Full Name</span>
-                          <span class="bc_font_alt_1 bc_text_20 bc_line_height_24 bc_text_normal bc_color_secondary my-2 d-block">Title</span>
-                          <div class="position-absolute detail p-3">
-                            <span class="bc_text_18 bc_line_height_30 text-white bc_font_default bc_text_normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </span>
-                          </div>
-                        </div>
-                      </div>
+					<?php } ?>  
                     </div>
                     <!-- Add Pagination -->
                     <div class="swiper-pagination team-pagination member-pagination d-md-none"></div>
                   </div>
                 </div>
+				<?php } ?>
             </div>
         </div>
     </div>
-    <?php echo do_shortcode('[bc-testimonial]');?>
-   
+			<?php 
+				if( have_rows('testimonials') ){
+			?>
+			<div class="container-fluid py-5 px-0 testimonials">
+				<div class="eagle_icon testimonial_icon position-absolute d-none d-md-block">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/icon-testimonials.png" class="img-fluid w-100 animated">
+				  </div>
+					<div class="container">
+						<div class="row no-gutters">
+						  <div class="col-lg-10 offset-lg-1 text-center pt-lg-5">
+							<span class="bc_text_28 bc_line_height_28 bc_font_default bc_color_primary bc_text_light bottom_line d-block position-relative mt-3 text-uppercase">testimonials</span>
+							<div class="col-lg-12">
+							  <div class="swiper-button-next testimonials-button-next d-none d-lg-block">
+								  <i class="far fa-chevron-right bc_text_30 bc_line_height_34 bc_color_primary"></i>
+							  </div>
+							  <div class="swiper-button-prev testimonials-button-prev d-none d-lg-block">
+								<i class="far fa-chevron-left  bc_text_30 bc_line_height_34 bc_color_primary"></i>
+							  </div>
+							  <div id="bc_testimonials-swiper_community" class="swiper-container testimonials-swiper mx-md-5 pb-3 mt-5">
+								<div class="swiper-wrapper pb-3">
+								<?php while( have_rows('testimonials') ){ the_row(); ?>
+									<div class="swiper-slide">
+										<p>“<?php echo  get_sub_field('testimonial');?>"</p>
+									</div>
+								<?php }?>
+								</div>
+								<!-- Add Pagination -->
+								<div class="swiper-pagination team-pagination testimonials-pagination"></div>
+							  </div>
+							</div>
+						  </div>
+						</div>
+					</div>
+			</div>
+			<?php }?>
  <?php get_template_part( 'page-templates/common/wondering-if-there' ); ?> 
 </main>
 <script type="text/javascript">
@@ -129,6 +126,32 @@ get_header();?>
         },
       }     
     });
-  
+  var swiperTestimonials = new Swiper('#bc_testimonials-swiper_community', {
+    navigation: {
+        nextEl: '.testimonials-button-next',
+        prevEl: '.testimonials-button-prev',
+    },
+     slidesPerView: 1,
+        loop: true,
+        speed: 400,
+        // autoplay: true,
+        paginationClickable: true,
+        spaceBetween: 50,
+        pagination: {
+            el: '.testimonials-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+      }     
+    }); 
 </script>
 <?php get_footer();?>
