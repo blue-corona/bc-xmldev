4d4667f45b6b7e604f03b2d02d301a494912ea97
Vishal Sharma <vsharma@bluecorona.com>
2020-05-28 16:53:45 +0530

    Ticket #20321 - style changes - Rv 14(only #3 point)

--- wp-content/themes/bc-xml/inc/widgets/bc-team-widget.php
+++ wp-content/themes/bc-xml/inc/widgets/bc-team-widget.php
@@ -79,12 +79,13 @@ class BC_teams_Widget extends WP_Widget {
 		?>
     	<!-- end -->
         </div>
-    	<div class="swiper-pagination team-pagination"></div>
+		<div class="swiper-pagination team-pagination"></div>
+		<div class="who_we_buttons">
+			<div class="swiper-button-next who_we_next bc_color_primary bc_text_30 bc_text_light bc_line_height_60"><i class="fal fa-2x fa-angle-right"></i></div>
+			<div class="swiper-button-prev who_we_prev bc_color_primary bc_text_30 bc_text_light bc_line_height_60"><i class="fal fa-2x fa-angle-left"></i></div>
+		</div>
 	</div>
-	<div class="who_we_buttons">
-	<div class="swiper-button-next who_we_next bc_color_primary bc_text_30 bc_text_light bc_line_height_60"><i class="fal fa-2x fa-angle-right"></i></div>
-	<div class="swiper-button-prev who_we_prev bc_color_primary bc_text_30 bc_text_light bc_line_height_60"><i class="fal fa-2x fa-angle-left"></i></div>
-	</div>
+		
      <a href="<?php echo get_home_url();?>/our-team/" class="btn_secondary bc_line_height_26 px-4 py-2 mt-3">FULL TEAM</a>
     </div>
 <?php echo $args['after_widget'];
