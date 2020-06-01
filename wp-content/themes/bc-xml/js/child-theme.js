ea4915b563a98ca08109d0e0946439b0b8df301a
Vishal Sharma <vsharma@bluecorona.com>
2020-06-01 11:27:20 +0530

    Ticket #20575 - Rev3, Rev9

--- wp-content/themes/bc-xml/js/child-theme.js
+++ wp-content/themes/bc-xml/js/child-theme.js
@@ -7141,3 +7141,29 @@ jQuery(function () {
     $if.attr("src", '/empty.html');
     $if.attr("src", src);
 });
+const $dropdown = jQuery(".dropdown");
+const $dropdownToggle = jQuery(".dropdown-toggle");
+const $dropdownMenu = jQuery(".dropdown-menu");
+const showClass = "show";
+ 
+jQuery(window).on("load resize", function() {
+  if (this.matchMedia("(min-width: 1025px)").matches) {
+	$dropdown.hover(
+	
+	  function() {
+		const $this = jQuery(this);
+		$this.addClass(showClass);
+		$this.find($dropdownToggle).attr("aria-expanded", "true");
+		$this.find($dropdownMenu).addClass(showClass);
+	  },
+	  function() {
+		const $this = jQuery(this);
+		$this.removeClass(showClass);
+		$this.find($dropdownToggle).attr("aria-expanded", "false");
+		$this.find($dropdownMenu).removeClass(showClass);
+	  }
+	);
+  } else {
+	$dropdown.off("mouseenter mouseleave");
+  }
+});
