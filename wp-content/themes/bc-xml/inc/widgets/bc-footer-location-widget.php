fd764f70f1d1ae784231185acd9c3b7c55164d93
Widya Susilo <wsusilo@bluecorona.com>
2020-06-01 13:52:42 -0400

    Post-launch QA change - footer phone number links fix

--- wp-content/themes/bc-xml/inc/widgets/bc-footer-location-widget.php
+++ wp-content/themes/bc-xml/inc/widgets/bc-footer-location-widget.php
@@ -20,16 +20,16 @@ class BC_Footer_Location_Widget extends WP_Widget {
         <span class="bc_text_26 bc_line_height_26 bc_font_default bc_text_light bc_color_primary">Locations</span>
         <span class="bc_sm_line_height_50">
             <span class="d-block bc_text_18 bc_line_height_40 bc_sm_line_height_50 bc_font_alt_1 bc_text_light bc_color_secondary mt-md-1 mt-3 text-uppercase">Rockville, MD</span>
-            <a href="tel:555-555-5555" class="d-block bc_text_18 bc_line_height_24 bc_font_alt_1 bc_text_light bc_color_primary bc_color_primary_hover mb-3 mb-md-1 no_hover_underline"> T: 301.770.5234</a>
-            <a href="tel:555-555-5555" class="d-block bc_text_18 bc_line_height_24 bc_font_alt_1 bc_text_light bc_color_primary bc_color_primary_hover mb-3 mb-md-1 no_hover_underline">F: 301.770.4929</a>
+            <a href="tel:3017705234" class="d-block bc_text_18 bc_line_height_24 bc_font_alt_1 bc_text_light bc_color_primary bc_color_primary_hover mb-3 mb-md-1 no_hover_underline"> T: 301.770.5234</a>
+            <a href="tel:3017704929" class="d-block bc_text_18 bc_line_height_24 bc_font_alt_1 bc_text_light bc_color_primary bc_color_primary_hover mb-3 mb-md-1 no_hover_underline">F: 301.770.4929</a>
             <span class="d-block bc_text_18 bc_line_height_40 bc_sm_line_height_50 bc_font_alt_1 bc_text_light bc_color_secondary mt-3 text-uppercase"> Falls Church, VA </span>
-            <a href="tel:555-555-5555" class="d-block bc_text_18 bc_line_height_24 bc_font_alt_1 bc_text_light bc_color_primary bc_color_primary_hover mb-3 mb-md-1 no_hover_underline"> T: 703.790.5999</a>
-            <a href="tel:555-555-5555" class="d-block bc_text_18 bc_line_height_24 bc_font_alt_1 bc_text_light bc_color_primary bc_color_primary_hover mb-3 mb-md-1 no_hover_underline"> LMA: 703.827.2300</a>
-            <a href="tel:555-555-5555" class="d-block bc_text_18 bc_line_height_24 bc_font_alt_1 bc_text_light bc_color_primary bc_color_primary_hover mb-3 mb-md-1 no_hover_underline">F: 703.749.8588</a>
-            <a href="tel:555-555-5555" class="d-block bc_text_18 bc_line_height_24 bc_font_alt_1 bc_text_light bc_color_primary bc_color_primary_hover mb-3 mb-md-1 no_hover_underline"> LMA: 704.827.2314</a>
+            <a href="tel:7037905999" class="d-block bc_text_18 bc_line_height_24 bc_font_alt_1 bc_text_light bc_color_primary bc_color_primary_hover mb-3 mb-md-1 no_hover_underline"> T: 703.790.5999</a>
+            <a href="tel:7038272300<" class="d-block bc_text_18 bc_line_height_24 bc_font_alt_1 bc_text_light bc_color_primary bc_color_primary_hover mb-3 mb-md-1 no_hover_underline"> LMA: 703.827.2300</a>
+            <a href="tel:7037498588" class="d-block bc_text_18 bc_line_height_24 bc_font_alt_1 bc_text_light bc_color_primary bc_color_primary_hover mb-3 mb-md-1 no_hover_underline">F: 703.749.8588</a>
+            <a href="tel:7048272314" class="d-block bc_text_18 bc_line_height_24 bc_font_alt_1 bc_text_light bc_color_primary bc_color_primary_hover mb-3 mb-md-1 no_hover_underline"> LMA: 704.827.2314</a>
             <span class="d-block bc_text_18 bc_line_height_40 bc_font_alt_1 bc_text_light bc_color_secondary mt-3 text-uppercase bc_sm_line_height_50"> Breckenridge, CO</span>
-            <a href="tel:555-555-5555" class="d-block bc_text_18 bc_line_height_24 bc_font_alt_1 bc_text_light bc_color_primary bc_color_primary_hover mb-3 mb-md-1 no_hover_underline"> T: 970.668.5700</a>
-            <a href="tel:555-555-5555" class="d-block bc_text_18 bc_line_height_24 bc_font_alt_1 bc_text_light bc_color_primary bc_color_primary_hover mb-3 mb-md-1 no_hover_underline"> F: 970.668.5701</a>
+            <a href="tel:9706685700" class="d-block bc_text_18 bc_line_height_24 bc_font_alt_1 bc_text_light bc_color_primary bc_color_primary_hover mb-3 mb-md-1 no_hover_underline"> T: 970.668.5700</a>
+            <a href="tel:9706685701" class="d-block bc_text_18 bc_line_height_24 bc_font_alt_1 bc_text_light bc_color_primary bc_color_primary_hover mb-3 mb-md-1 no_hover_underline"> F: 970.668.5701</a>
         </span>
    
     <?php echo $args['after_widget'];
